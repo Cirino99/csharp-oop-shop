c1619752b54f2b33aaa664c1f43f53a3cc965e83
Simone <cirino.simone99@gmail.com>
2022-11-03 11:27:10 +0100

    rifatta classe prodotto usando le property

--- Program.cs
+++ Program.cs
@@ -16,85 +16,141 @@
 //Nella vostro programma principale, testate tutte le funzionalità della classe Prodotto.
 //BONUS: create un metodo che restituisca il codice con un pad left di 0 per arrivare a 8 caratteri (ad esempio codice 91 diventa 00000091, mentre codice 123445567 resta come è)
 
+//Prodotto prodotto1 = new Prodotto();
+//prodotto1.SetNome("Ipad");
+//prodotto1.SetDescrizione("Nuovo ipad");
+//prodotto1.SetPrezzo(349.99);
+//prodotto1.SetIva(22);
+//Console.WriteLine(prodotto1.GetCodice());
+//Console.WriteLine(prodotto1.GetNome());
+//Console.WriteLine(prodotto1.GetDescrizione());
+//Console.WriteLine(prodotto1.GetPrezzo());
+//Console.WriteLine(prodotto1.GetIva());
+//Console.WriteLine(prodotto1.GetPrezzoIva());
+//Console.WriteLine(prodotto1.GetNomeEsteso());
+//Console.WriteLine(prodotto1.GetCodicePadLeft());
+
+//public class Prodotto
+//{
+//    private int codice;
+//    private string nome;
+//    private string descrizione;
+//    private double prezzo;
+//    private int iva;
+
+//    public Prodotto()
+//    {
+//        Random random = new Random();
+//        codice = random.Next(1, 12345678);
+//    }
+
+//    public int GetCodice()
+//    {
+//        return codice;
+//    }
+//    public void SetNome(string nome)
+//    {
+//        this.nome = nome;
+//    }
+//    public string GetNome()
+//    {
+//        return nome;
+//    }
+//    public void SetDescrizione(string descrizione)
+//    {
+//        this.descrizione = descrizione;
+//    }
+//    public string GetDescrizione()
+//    {
+//        return descrizione;
+//    }
+//    public void SetPrezzo(double prezzo)
+//    {
+//        this.prezzo = prezzo;
+//    }
+//    public double GetPrezzo()
+//    {
+//        return prezzo;
+//    }
+//    public void SetIva(int iva)
+//    {
+//        this.iva = iva;
+//    }
+//    public int GetIva()
+//    {
+//        return iva;
+//    }
+//    public double GetPrezzoIva()
+//    {
+//        return prezzo + (prezzo / 100 * iva);
+//    }
+//    public string GetNomeEsteso()
+//    {
+//        return codice + " " + nome;
+//    }
+//    public string GetCodicePadLeft()
+//    {
+//        string codice = Convert.ToString(this.codice);
+//        while (codice.Length < 8)
+//        {
+//            codice = '0' + codice;
+//        }
+//        return codice;
+//    }
+//}
+
+//fatto con le property
+
 Prodotto prodotto1 = new Prodotto();
-prodotto1.SetNome("Ipad");
-prodotto1.SetDescrizione("Nuovo ipad");
-prodotto1.SetPrezzo(349.99);
-prodotto1.SetIva(22);
-Console.WriteLine(prodotto1.GetCodice());
-Console.WriteLine(prodotto1.GetNome());
-Console.WriteLine(prodotto1.GetDescrizione());
-Console.WriteLine(prodotto1.GetPrezzo());
-Console.WriteLine(prodotto1.GetIva());
-Console.WriteLine(prodotto1.GetPrezzoIva());
-Console.WriteLine(prodotto1.GetNomeEsteso());
-Console.WriteLine(prodotto1.GetCodicePadLeft());
+prodotto1.Nome = "Ipad";
+prodotto1.Descrizione = "Nuovo ipad";
+prodotto1.Prezzo =  349.99;
+prodotto1.Iva = 22;
+Console.WriteLine(prodotto1.Codice);
+Console.WriteLine(prodotto1.Nome);
+Console.WriteLine(prodotto1.Descrizione);
+Console.WriteLine(prodotto1.Prezzo);
+Console.WriteLine(prodotto1.Iva);
+Console.WriteLine(prodotto1.PrezzoIva);
+Console.WriteLine(prodotto1.NomeEsteso);
+Console.WriteLine(prodotto1.CodicePadLeft);
 
 public class Prodotto
 {
-    private int codice;
-    private string nome;
-    private string descrizione;
-    private double prezzo;
-    private int iva;
-
     public Prodotto()
     {
         Random random = new Random();
-        codice = random.Next(1, 12345678);
+        Codice = random.Next(1, 12345678);
+    }
+    public int Codice { get; }
+    public string Nome { get; set; }
+    public string Descrizione { get; set; }
+    public double Prezzo { get; set; }
+    public int Iva { get; set; }
+    public double PrezzoIva
+    {
+        get
+        {
+            return Prezzo + (Prezzo / 100 * Iva);
+        }
+    }
+    public string NomeEsteso { 
+        get
+        {
+            return Codice + " " + Nome;
+        }
+    }
+    public string CodicePadLeft
+    {
+        get
+        {
+            string codice = Convert.ToString(Codice);
+            while (codice.Length < 8)
+            {
+                codice = '0' + codice;
+            }
+            return codice;
+        }
     }
 
-    public int GetCodice()
-    {
-        return codice;
-    }
-    public void SetNome(string nome)
-    {
-        this.nome = nome;
-    }
-    public string GetNome()
-    {
-        return nome;
-    }
-    public void SetDescrizione(string descrizione)
-    {
-        this.descrizione = descrizione;
-    }
-    public string GetDescrizione()
-    {
-        return descrizione;
-    }
-    public void SetPrezzo(double prezzo)
-    {
-        this.prezzo = prezzo;
-    }
-    public double GetPrezzo()
-    {
-        return prezzo;
-    }
-    public void SetIva(int iva)
-    {
-        this.iva = iva;
-    }
-    public int GetIva()
-    {
-        return iva;
-    }
-    public double GetPrezzoIva()
-    {
-        return prezzo + (prezzo / 100 * iva);
-    }
-    public string GetNomeEsteso()
-    {
-        return codice + " " + nome;
-    }
-    public string GetCodicePadLeft()
-    {
-        string codice = Convert.ToString(this.codice);
-        while (codice.Length < 8)
-        {
-            codice = '0' + codice;
-        }
-        return codice;
-    }
 }
